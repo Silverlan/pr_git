53588715a54ea5964fedcb69fb90d5af12a5e5e1
Silverlan <silverlan@pragma-engine.com>
2025-11-18 20:35:36 +0100

    refactor: migrate library to C++20 modules

--- include/pr_git.hpp
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: (c) 2021 Silverlan <opensource@pragma-engine.com>
-// SPDX-License-Identifier: MIT
-
-#ifndef __PR_GIT_HPP__
-#define __PR_GIT_HPP__
-
-#include <luasystem.h>
-
-#endif
